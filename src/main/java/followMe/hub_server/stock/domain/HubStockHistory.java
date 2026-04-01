5e53d36ca0f2fde5e62153b435d6b4285f689d2a
jsh9057 <jsh9057@gmail.com>
2026-04-01 14:37:39 +0900

    feat: stock-history 초기 도메인 정의

---
+++ src/main/java/followMe/hub_server/stock/domain/HubStockHistory.java
@@ -0,0 +1,79 @@
+package followMe.hub_server.stock.domain;
+
+import com.followMe.common.entity.BaseAudit;
+import followMe.hub_server.stock.domain.vo.ProductInfo;
+import followMe.hub_server.stock.domain.vo.Vendor;
+import jakarta.persistence.*;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
+
+@Getter
+@Entity
+@Table(name = "p_hub_stock_history")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLRestriction("deleted_at IS NULL")
+public class HubStockHistory extends BaseAudit {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(nullable = false)
+  private UUID historyId;
+
+  @Column(nullable = false)
+  private UUID productId;
+
+  @Embedded private ProductInfo productInfo;
+
+  @Embedded private Vendor vendor;
+
+  @Column(nullable = false)
+  private UUID hubId;
+
+  @Column(nullable = false)
+  private Type type;
+
+  private UUID refId;
+
+  @Column(nullable = false)
+  private Integer beforeQuantity;
+
+  @Column(nullable = false)
+  private Integer afterQuantity;
+
+  @Builder(access = AccessLevel.PRIVATE)
+  public HubStockHistory(
+      UUID productId,
+      ProductInfo productInfo,
+      Vendor vendor,
+      UUID hubId,
+      Type type,
+      UUID refId,
+      Integer beforeQuantity,
+      Integer afterQuantity) {
+    this.productId = productId;
+    this.productInfo = productInfo;
+    this.vendor = vendor;
+    this.hubId = hubId;
+    this.type = type;
+    this.refId = refId;
+    this.beforeQuantity = beforeQuantity;
+    this.afterQuantity = afterQuantity;
+  }
+
+  public static HubStockHistory recode(
+      HubStock hubStock, Type type, UUID refId, Integer beforeQuantity, Integer afterQuantity) {
+    return HubStockHistory.builder()
+        .productId(hubStock.getProductId())
+        .productInfo(hubStock.getProductInfo())
+        .vendor(hubStock.getVendor())
+        .hubId(hubStock.getHubId())
+        .type(type)
+        .refId(refId)
+        .beforeQuantity(beforeQuantity)
+        .afterQuantity(afterQuantity)
+        .build();
+  }
+}
