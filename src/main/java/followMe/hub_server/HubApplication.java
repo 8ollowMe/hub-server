6f90fbfcfbb8715b55d126a75e909a4939fc432a
jsh9057 <jsh9057@gmail.com>
2026-04-06 11:17:28 +0900

    fix: conflict solve

--- src/main/java/followMe/hub_server/HubApplication.java
+++ src/main/java/followMe/hub_server/HubApplication.java
@@ -2,29 +2,20 @@ package followMe.hub_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-=======
 import org.springframework.cache.annotation.EnableCaching;
->>>>>>> a94c7eb9e7bc1f238d2d56f6b6b90a87ea516ef3
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-<<<<<<< HEAD
+@EnableCaching
 @EnableScheduling
 @EnableFeignClients
 @EnableJpaAuditing
 @EnableJpaRepositories(basePackages = {"com.followMe", "followMe.hub_server"})
 @EntityScan(basePackages = {"com.followMe", "followMe.hub_server"})
 @SpringBootApplication(scanBasePackages = {"com.followMe", "followMe.hub_server"})
-=======
-@EnableCaching
-@EnableJpaAuditing
-@EnableFeignClients
-@SpringBootApplication
->>>>>>> a94c7eb9e7bc1f238d2d56f6b6b90a87ea516ef3
 public class HubApplication {
 
   public static void main(String[] args) {
