076169600b2e206ed1060c18568eb32f090f76c3
juneon <juneon0830@naver.com>
2026-04-06 15:40:06 +0900

    spotless 적용

--- src/main/java/followMe/hub_server/common/config/SwaggerConfig.java
+++ src/main/java/followMe/hub_server/common/config/SwaggerConfig.java
@@ -8,12 +8,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI hubOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Hub Server API")
-                        .version("v1")
-                        .description("허브 서버 API 문서"));
-    }
+  @Bean
+  public OpenAPI hubOpenAPI() {
+    return new OpenAPI()
+        .info(new Info().title("Hub Server API").version("v1").description("허브 서버 API 문서"));
+  }
 }
