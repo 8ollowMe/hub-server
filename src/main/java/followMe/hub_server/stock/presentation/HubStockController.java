3cee976e888de120f180510d34d8431b288d70b5
jsh9057 <jsh9057@gmail.com>
2026-04-07 00:46:23 +0900

    docs: Swagger API 명세 작성

--- src/main/java/followMe/hub_server/stock/presentation/HubStockController.java
+++ src/main/java/followMe/hub_server/stock/presentation/HubStockController.java
@@ -14,12 +14,15 @@ import followMe.hub_server.stock.application.service.HubStockService;
 import followMe.hub_server.stock.application.service.QueryHubStockService;
 import followMe.hub_server.stock.application.service.UserContext;
 import followMe.hub_server.stock.application.service.UserRole;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "재고", description = "허브 재고 관리 관련 외부 API")
 @RestController
 @RequestMapping("/api/v1/stocks")
 @RequiredArgsConstructor
@@ -37,6 +40,11 @@ public class HubStockController {
     return new UserContext(userId, role, hubId, vendorId, userName);
   }
 
+  @Operation(
+      summary = "재고 등록",
+      description =
+          "초기 상품 재고를 등록합니다.<br>'MASTER', 'HUB(담당 허브)' 권한을 가진 사용자만 접근 가능합니다."
+              + "<br>재고의 생성, 수정, 삭제 모든 과정은 stock-history를 통해 기록됩니다.")
   @PostMapping
   public ResponseEntity<ApiResponse> createHubStock(
       @RequestBody CreateHubStockRequest request, @ModelAttribute UserContext authUser) {
@@ -45,6 +53,12 @@ public class HubStockController {
     return ApiResponse.ok(response);
   }
 
+  @Operation(
+      summary = "재고 조회",
+      description =
+          "재고들을 조회, 검색 합니다. "
+              + "<br>로그인 한 사용자만 접근 가능합니다. "
+              + "<br>상품 식별자, 허브 식별자, 업체 식별자 별로 검색 조회를 제공합니다.")
   @GetMapping
   public ResponseEntity<ApiResponse> getHubStocks(
       SearchCondition condition, PageRequest pageRequest, @ModelAttribute UserContext authUser) {
@@ -54,6 +68,12 @@ public class HubStockController {
     return ApiResponse.ok(PageResponse.of(response));
   }
 
+  @Operation(
+      summary = "재고 변경",
+      description =
+          "재고 입고, 출고 내역(INBOUND(입고), OUTBOUND(출고), ADJUST_LOSS(파손), RETURN_IN(반품), DISCONTINUED(단종), ORDER_CANCELED(주문 취소))을"
+              + "<br> 기록하고, 재고를 수정합니다."
+              + "<br>'MASTER', 'HUB(담당 허브)' 권한을 가진 사용자만 접근 가능합니다.")
   @PatchMapping("/{productId}")
   public ResponseEntity<ApiResponse> updateHubStock(
       @PathVariable UUID productId,
@@ -64,6 +84,12 @@ public class HubStockController {
     return ApiResponse.ok(response);
   }
 
+  @Operation(
+      summary = "재고 삭제",
+      description =
+          "재고 목록에서 상품을 삭제합니다."
+              + "<br> 재고가 남아있는 상품은 삭제 할 수 없습니다."
+              + "<br>'MASTER', 'HUB(담당 허브)' 권한을 가진 사용자만 접근 가능합니다.")
   @DeleteMapping("/{productId}")
   public ResponseEntity<ApiResponse> deleteHubStock(
       @PathVariable UUID productId,
--- src/main/java/followMe/hub_server/stock/presentation/InternalHubStockController.java
+++ src/main/java/followMe/hub_server/stock/presentation/InternalHubStockController.java
@@ -9,11 +9,14 @@ import followMe.hub_server.stock.application.service.HubStockService;
 import followMe.hub_server.stock.application.service.QueryHubStockService;
 import followMe.hub_server.stock.application.service.UserContext;
 import followMe.hub_server.stock.application.service.UserRole;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "재고 내부 API", description = "허브 재고 관리 관련 내부 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/internal/v1/stocks")
@@ -31,6 +34,9 @@ public class InternalHubStockController {
     return new UserContext(userId, role, hubId, vendorId, userName);
   }
 
+  @Operation(
+      summary = "재고 다건 조회",
+      description = "상품 식별자 리스트를 통해 상품의 재고 리스트를 조회 합니다." + "<br>로그인한 모든 사용자가 접근 가능합니다.")
   @GetMapping
   public List<SearchResponse> getStocks(
       @RequestBody SearchProductIdsRequest request,
@@ -41,6 +47,9 @@ public class InternalHubStockController {
     return responses;
   }
 
+  @Operation(
+      summary = "주문을 통한 재고 다건 변경",
+      description = "주문 식별자, 상품 리스트, 상품 수량 리스트를 통해 다건의 재고를 감소 시킵니다." + "<br>로그인한 모든 사용자가 접근 가능합니다.")
   @PatchMapping("/order")
   public OrderHubStockResponse requestStockOrder(
       @RequestBody OrderHubStockRequest request, @ModelAttribute UserContext authUser) {
@@ -49,6 +58,9 @@ public class InternalHubStockController {
     return response;
   }
 
+  @Operation(
+      summary = "주문 취소를 통한 재고 다건 변경",
+      description = "주문 식별자, 상품 리스트, 상품 수량 리스트를 통해 다건의 재고를 복구 합니다." + "<br>로그인한 모든 사용자가 접근 가능합니다.")
   @PatchMapping("/cancel")
   public OrderHubStockResponse cancelStockOrder(
       @RequestBody OrderHubStockRequest request, @ModelAttribute UserContext authUser) {
