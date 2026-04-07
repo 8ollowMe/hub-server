f692226965e5cf1dcd3c6a3b4d7eb4dabba9e101
juneon <juneon0830@naver.com>
2026-04-07 16:52:36 +0900

    feat: 각 이동 정보간 예상 시간 및 예상 거리 추가

--- src/main/java/followMe/hub_server/hub/application/dto/result/RouteNodeResult.java
+++ src/main/java/followMe/hub_server/hub/application/dto/result/RouteNodeResult.java
@@ -2,7 +2,8 @@ package followMe.hub_server.hub.application.dto.result;
 
 import followMe.hub_server.hub.application.dto.enums.NodeType;
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.UUID;
 
-public record RouteNodeResult(UUID id, NodeType type, String name, int sequence)
+public record RouteNodeResult(UUID id, NodeType type, String name, BigDecimal duration, BigDecimal distance, int sequence)
     implements Serializable {}
